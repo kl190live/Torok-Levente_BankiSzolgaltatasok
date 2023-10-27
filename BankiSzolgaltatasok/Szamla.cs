2b92892094f496b880a7cada8f522010474a2ece
Levente Török <tlevi0409@gmail.com>
2023-10-27 14:42:39 +0200

    A Szamla class megcsinaltam es megneztem hogy a teszt eddig mukodik

---
+++ BankiSzolgaltatasok/Szamla.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    public abstract class Szamla : BankiSzolgaltatas
+    {
+        protected int aktualisEgyenleg;
+
+        public Szamla(Tulajdonos tulajdonos) : base(tulajdonos)
+        {
+
+        }
+
+        public int AktualisEgyenleg { get => aktualisEgyenleg; }
+
+        public void Befizet(int osszeg)
+        {
+            aktualisEgyenleg += osszeg;
+        }
+
+        public abstract bool Kivesz(int osszeg);
+
+    }
+}
