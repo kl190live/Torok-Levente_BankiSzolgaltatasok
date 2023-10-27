6bbca4de0bdf682954c2c3cf45c7b34139b1752c
Levente Török <tlevi0409@gmail.com>
2023-10-27 14:45:11 +0200

    A kartya osztalyt és az ujkartyat megcsinaltam

---
+++ BankiSzolgaltatasok/Kartya.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    public class Kartya : BankiSzolgaltatas
+    {
+        private string kartyaSzam;
+        private Szamla szamla;
+
+        public Kartya(Tulajdonos tulajdonos, Szamla szamla, string kartyaSzam) : base(tulajdonos)
+        {
+            this.kartyaSzam = kartyaSzam;
+            this.szamla = szamla;
+        }
+
+        public string KartyaSzam { get => kartyaSzam; }
+
+        public bool Vasarlas(int osszeg)
+        {
+            if (osszeg > 0)
+            {
+                szamla.Kivesz(osszeg);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
