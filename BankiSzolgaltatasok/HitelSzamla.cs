95d5658a73a073be28a8a342ec2510452749a9de
Levente Török <tlevi0409@gmail.com>
2023-10-27 14:43:30 +0200

    A hitelszamlat class megcsinaltam

---
+++ BankiSzolgaltatasok/HitelSzamla.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankiSzolgaltatasok
+{
+    public class HitelSzamla : Szamla
+    {
+        private int hitelKeret;
+
+        public HitelSzamla(Tulajdonos tulajdonos, int hitelKeret) : base(tulajdonos)
+        {
+            this.hitelKeret = hitelKeret;
+
+        }
+
+        public int HitelKeret { get => hitelKeret; }
+
+        public override bool Kivesz(int osszeg)
+        {
+            {
+                if (aktualisEgyenleg < hitelKeret || osszeg == hitelKeret)
+                {
+                    hitelKeret -= osszeg;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Nincs annyi pénz a számládon!");
+                    return false;
+                }
+            }
+        }
+    }
+}
